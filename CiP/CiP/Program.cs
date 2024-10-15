1031fabf3df55695cd305a1ae457965623641f8e
Gustav Franchell <gfranchell@gmail.com>
2024-10-15 09:26:08 +0200

    Update Program.cs to execute functions

--- CiP/CiP/Program.cs
+++ CiP/CiP/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CiP;
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+
+        // Now you can access the ReadFile class from the CiP namespace
+        ReadFile.ReadFileFunction(); // This calls the Main() method in the ReadFile class
+        Routes.RouteCalculator();
+    }
+}
+
