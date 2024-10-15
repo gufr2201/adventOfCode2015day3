1d1f9162a660645ca99cdc51fc4b6b0ddabe63c8
Gustav Franchell <gfranchell@gmail.com>
2024-10-15 09:26:29 +0200

    Add function to calculate santas routes

---
+++ CiP/CiP/Routes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace CiP
+{
+    public class Routes
+    {
+        public static void RouteCalculator()
+        {
+           string directions = ReadFile.ReadFileFunction(); 
+            HashSet<(int, int)> visitedHouses =new HashSet<(int, int)> ();
+            int x = 0;
+            int y = 0;
+            visitedHouses.Add((x, y));
+
+            foreach (char direction in directions)
+            {
+                switch (direction)
+                {
+                    case '^': y++; break;
+                    case 'v': y--; break;
+                    case '>': x++; break;
+                    case '<': x--; break;
+
+                }
+                visitedHouses.Add((x, y));
+            }
+            Console.WriteLine($"amount of houses that got at least one christmas present: {visitedHouses.Count}");
+        }
+    }
+}
