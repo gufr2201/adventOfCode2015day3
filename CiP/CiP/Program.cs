337d7eed0276faec889a0c90b1d9797e269fb5c3
Gustav Franchell <gfranchell@gmail.com>
2024-10-17 13:22:55 +0200

    Create routes for santa part 1

--- CiP/CiP/Program.cs
+++ CiP/CiP/Program.cs
@@ -8,8 +8,8 @@ class Program
     {
 
         // Now you can access the ReadFile class from the CiP namespace
-        ReadFile.ReadFileFunction(); // This calls the Main() method in the ReadFile class
-        Routes.RouteCalculator();
+        string directions = ReadFile.ReadFileFunction(); // This calls the Main() method in the ReadFile class
+        Routes.RouteCalculator(directions);
     }
 }
 
--- CiP/CiP/Routes.cs
+++ CiP/CiP/Routes.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 using System.IO;
 namespace CiP
 {
+
     public class Routes
     {
-        public static void RouteCalculator()
+        string directions = ReadFile.ReadFileFunction();
+        public static int RouteCalculator(string directions)
         {
-           string directions = ReadFile.ReadFileFunction(); 
-            HashSet<(int, int)> visitedHouses =new HashSet<(int, int)> ();
+            HashSet<(int, int)> visitedHouses = new HashSet<(int, int)>();
             int x = 0;
             int y = 0;
             visitedHouses.Add((x, y));
@@ -29,6 +30,65 @@ namespace CiP
                 visitedHouses.Add((x, y));
             }
             Console.WriteLine($"amount of houses that got at least one christmas present: {visitedHouses.Count}");
+            return visitedHouses.Count;
         }
     }
 }
+
+//    public interface IDeliveryEntity
+//    {
+//        void Move(char direction);
+//        (int X, int Y) Position { get; }
+//    }
+//    public class Santa : IDeliveryEntity
+//    {
+//        public int X { get; private set; }
+//        public int Y { get; private set; }
+//        public (int X, int Y) Position => (X, Y);
+
+//        public void Move(char direction)
+//        {
+//            switch (direction)
+//            {
+//                case '^': Y++; break;
+//                case 'v': Y--; break;
+//                case '>': X++; break;
+//                case '<': Y--; break;
+//            }
+//        }
+//        public class RoboSanta : IDeliveryEntity
+//        {
+//            public int X { get; private set; }
+//            public int Y { get; private set; }
+//            public (int X, int Y) Position => (X, Y);
+
+//            public void Move(char direction)
+//            {
+//                switch (direction)
+//                {
+//                    case '^': Y--; break;
+//                    case 'v': Y++; break;
+//                    case '>': X--; break;
+//                    case '<': Y++; break;
+//                }
+//            }
+//            public class RouteCalculator
+//            {
+//                public HashSet<(int, int)> CalculateVisitedHouses(string directions, params IDeliveryEntity[] entities)
+//                {
+//                    var visitedHouses = new HashSet<(int, int)>();
+//                    visitedHouses.Add((0, 0)); //starting house
+
+//                    int entityIndex = 0;
+//                    foreach (char direction in directions)
+//                    {
+//                        entities[entityIndex].Move(direction);
+//                        visitedHouses.Add(entities[entityIndex].Position);
+//                        entityIndex = (entityIndex + 1) % entities.Length;
+//                    }
+//                    return visitedHouses;
+//                }
+//            }
+//        }
+//    }
+//}
